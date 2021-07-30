99bd92ad4f65d148009244d35dff757a17f0560a
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-07-30 22:32:19 +1000

    Start configuring auth

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,6 +1,15 @@
 import App from "../App";
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 
 function Firebase(props) {
+    const firebase = props.firebase
+  /*  let ui = new firebaseui.auth.AuthUI(firebase.auth())
+    ui.start('#firebaseui-auth-container', {
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ]
+    })*/
     return (<App/>)
 }
 
