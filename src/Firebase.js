5ebed18910450c9dbee134ab2a01cdeec0053374
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-08-02 22:57:15 +1000

    Configure appcheck

--- src/Firebase.js
+++ src/Firebase.js
@@ -15,4 +15,7 @@ const firebaseConfig = {
 
 Firebase.initializeApp(firebaseConfig)
 
+const appCheck = Firebase.appCheck();
+appCheck.activate(process.env.APP_CHECK_PUBLIC_KEY, true)
+
 export default Firebase
