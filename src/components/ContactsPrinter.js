0e4a346a39288ed39f11fb706bdb361b7e3e9efa
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-08-16 20:28:56 +1000

    Format contacts and add to PDF

--- src/components/ContactsPrinter.js
+++ src/components/ContactsPrinter.js
@@ -1,12 +1,38 @@
 import {Button} from "react-bootstrap";
+import {jsPDF} from "jspdf";
 
 function ContactsPrinter(props) {
-    function printContacts(e){
-        console.log(props.contactsToPrint);
+    function formatContacts(contacts) {
+        return contacts.map((contact) =>
+            `
+            ${contact.name}
+            ${contact.address}
+            Ph: ${contact.phone}
+            
+            
+            `
+        ).join("");
+    }
+
+    function printContacts(e) {
+        const doc = new jsPDF();
+        doc.setFontSize(14);
+        let column1 = [];
+        let column2 = [];
+        props.contactsToPrint.forEach((contact, index) => {
+            if ((index % 2) === 0) {
+                column1.push(contact);
+            } else {
+                column2.push(contact);
+            }
+        })
+        doc.text(formatContacts(column1), 5, 10);
+        doc.text(formatContacts(column2), 100, 10);
+        doc.save("contacts.pdf");
     }
 
     return (
-        <Button variant="success" onClick={(e) => printContacts(e)}>Print</Button>
+        <Button variant="success" onClick={(e) => printContacts(e)}>Save to print list</Button>
     );
 }
 
